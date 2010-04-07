bcc58d767b0e30db1ac5976c236c04b9201d58d6
Ryan Shaw <ryankshaw@gmail.com>
2010-04-06 23:39:10 -0600

    clean up code, match input's font & font-size

--- html5placeholder.jquery.js
+++ html5placeholder.jquery.js
@@ -1,5 +1,6 @@
 // HTML5 placeholder plugin version 0.3
-// Copyright (c) 2010-The End of Time, Mike Taylor, http://miketaylr.com
+// find me at: http://github.com/ryankshaw/jQuery-html5-placeholder
+// Original version Copyright (c) 2010-The End of Time, Mike Taylor, http://miketaylr.com
 // MIT Licensed: http://www.opensource.org/licenses/mit-license.php
 //
 // Enables cross-browser* html5 placeholder for inputs, by first testing
@@ -14,57 +15,52 @@
 (function($){
   
   $.fn.placeholder = function(options) {
-    //merge in passed in options, if any
-    var opts = $.extend($.fn.placeholder.defaults, options),
-    //cache the original 'left' value, for use by Opera later
-    o_left = opts.placeholderCSS.left;
+    var opts = $.extend($.fn.placeholder.defaults, options);
   
-    //sniffy sniff sniff
-    var isOpera = function() {
-      return $.browser.opera;
-    };
-  
-    //first test for native placeholder support before continuing
-    //feature detection inspired by ye olde jquery 1.4 hawtness, with paul irish
-    return ('placeholder' in document.createElement('input')) ? this : this.each(function() {
-    
-      //local vars
-      var $this = $(this),
-          inputVal = $.trim($this.val()),
-          inputWidth = $this.width(),
-          inputHeight = $this.height(),
-          //grab the inputs id for the <label for>, or make a new one from the Date
-          //ids can start with numbers in html5, btw
-          inputId = ($this.attr('id') !== '') ? $this.attr('id') : + new Date(),
-          placeholderText = $this.attr('placeholder'),
-          placeholder = $('<label for='+ inputId +'>'+ placeholderText + '</label>');
-        
-          //stuff in some calculated values into the placeholderCSS object
-          opts.placeholderCSS['width'] = inputWidth;
-          opts.placeholderCSS['height'] = inputHeight;
-          // adjust position of placeholder to accomodate opera's super ugly 'email' and 'url' graphics
-          opts.placeholderCSS.left = (isOpera() && this.getAttribute('type')==='email' || isOpera() && this.getAttribute('type')==='url') ? '11%' : o_left;
-          placeholder.css(opts.placeholderCSS);
-    
-      //place the placeholder if the input is empty
-      if (!inputVal){
-        $this.wrap(opts.inputWrapper);
-        $this.attr('id', inputId).after(placeholder);
-      };
-    
-      //hide placeholder on focus
-      $this.focus(function(){
-        if (!$.trim($this.val())){
-         $this.next().hide();
-        };
-      });
-    
-      //show placeholder if the input is empty
-      $this.blur(function(){
-        if (!$.trim($this.val())){
-          $this.next().show();
-        };
-      });
+    return this.each(function() {
+      //first test for native placeholder support before continuing
+      //feature detection inspired by ye olde jquery 1.4 hawtness, with paul irish
+      // Added support for <textarea>s in browsers that support the placeholder tag only on input elements (safari)
+      if ( "placeholder" === typeof(document.createElement(this.tagName.toLowerCase()).placeholder)) {
+        var $this = $(this),
+            //grab the inputs id for the <label for>, or make a new one from the Date
+            //ids can start with numbers in html5, btw
+            inputId = $this.attr('id') || ('id'+ new Date().getTime()),
+            $label = $('<label/>', {
+              'for' : inputId,
+              html  : $this.attr('placeholder'),
+              css   : $.extend({}, opts.placeholderCSS, {
+                //stuff in some calculated values into the placeholderCSS object
+                'font-family' : $this.css('font-family'),
+                'font-size'   : $this.css('font-size'),
+                'color'       : $this.attr(''),
+                'width'       : $this.width(),
+                'height'      : $this.height(),
+                // adjust position of placeholder to accomodate opera's super ugly 'email' and 'url' graphics
+                'left'        : $.browser.opera && ($this.attr('type') === 'email' || $this.attr('type') === 'url') ? '11%' : opts.placeholderCSS.left
+              }),
+              click : function(){
+                $this.focus(); //if you click the label, focus the input
+              }
+            })[$.trim($this.val()) ? 'hide' : 'show' ](); // Show or hide the placeholder label depending on if there is text in the input.
+      
+        $this
+          .wrap(opts.inputWrapper)
+          .attr('id', inputId)
+          //hide placeholder on focus
+          .focus(function(){
+            if (!$.trim($this.val())){
+             $label.hide();
+            };
+          })
+          //show placeholder if the input is empty
+          .blur(function(){
+            if (!$.trim($this.val())){
+              $label.show();
+            };
+          })
+          .after($label);
+      }
     });
   };
   
@@ -76,12 +72,12 @@
     //more or less just emulating what webkit does here
     //tweak to your hearts content
     placeholderCSS: {
-      'font':'0.75em sans-serif', 
-      'color':'#bababa', 
-      'position': 'absolute', 
-      'left':'5px',
-      'top':'20%', //basically for Opera, top:0 works for everyone else :/
-      'overflow-x': 'hidden'
+      'position'    : 'absolute', 
+      'color'       : '#aaa',
+      'left'        : '5px',
+      'line-height' : '1em',
+      'top'         : '20%', //basically for Opera, top:0 works for everyone else :/
+      'overflow-x'  : 'hidden'
     }
   };
 })(jQuery);
